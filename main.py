be44ee92ecf60e29729cb59d8f6c0f27d82f0591
Aleksey525 <alex_tolchin@mail.ru>
2024-01-21 00:11:19 +0300

    upgrade main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import requests
 from urllib.parse import urlsplit
 from dotenv import load_dotenv
 import os
-import json
-
-
-token = os.environ['BITLY_TOKEN']
 
 
 def shorten_link(token: str, url: str) -> str:
@@ -17,18 +13,13 @@ def shorten_link(token: str, url: str) -> str:
     return response.json()['id']
 
 
-def valid_url(link: str) -> None:
-    response = requests.get(link)
-    response.raise_for_status()
-    return None
-
-
 def count_clicks(token: str, bitlink: str) -> str:
     headers = {'Authorization': token}
     payload = {'units': '-1'}
     url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
     splited_url = urlsplit(bitlink)
-    complite_url = url_template.format(splited_url[1] + splited_url[2])
+    bitlink_atr = f'{splited_url.netloc}{splited_url.path}'
+    complite_url = url_template.format(bitlink_atr)
     response = requests.get(complite_url, params=payload, headers=headers)
     response.raise_for_status()
     return response.json()['total_clicks']
@@ -36,27 +27,32 @@ def count_clicks(token: str, bitlink: str) -> str:
 
 def is_bitlink(url: str) -> bool:
     is_bitlink = False
-    test = urlsplit(url)
-    if test[1] == 'bit.ly':
+    splited_url = urlsplit(url)
+    url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
+    url_atr = f'{splited_url.netloc}{splited_url.path}'
+    complite_url = url_template.format(url_atr)
+    headers = {'Authorization': token}
+    response = requests.get(complite_url, headers=headers)
+    if response.ok:
         is_bitlink = True
     return is_bitlink
+
+
 if __name__ == '__main__':
     load_dotenv()
     token = os.environ['BITLY_TOKEN']
     user_url = input('Введите ссылку: ')
     if is_bitlink(user_url):
         try:
-            valid = valid_url(user_url)
             count_clicks = count_clicks(token, user_url)
         except requests.exceptions.ConnectionError:
             print(f'Введен некорректный адрес')
         except requests.exceptions.HTTPError:
-            print(f'Запрос завершился неудачно')
+            print(f'Запрос завершился неудачно!')
         else:
             print(f'По вашей ссылке прошли {count_clicks} раз(а)')
     else:
         try:
-            valid = valid_url(user_url)
             shorten_bitlink = shorten_link(token, user_url)
         except requests.exceptions.ConnectionError:
             print(f'Введен некорректный адрес')
