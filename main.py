c7816a2f83b381a5aa7de6e95af45415c080d285
Aleksey525 <alex_tolchin@mail.ru>
2024-01-21 20:02:02 +0300

    upgrade main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def shorten_link(token: str, url: str) -> str:
     return response.json()['id']
 
 
-def count_clicks(token: str, bitlink: str) -> str:
+def count_clicks(bitlink: str, token: str) -> str:
     headers = {'Authorization': token}
     payload = {'units': '-1'}
     url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
@@ -25,30 +25,27 @@ def count_clicks(token: str, bitlink: str) -> str:
     return response.json()['total_clicks']
 
 
-def is_bitlink(url: str) -> bool:
-    is_bitlink = False
+def is_bitlink(url: str, token: str) -> bool:
     splited_url = urlsplit(url)
     url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
     url_atr = f'{splited_url.netloc}{splited_url.path}'
     complite_url = url_template.format(url_atr)
     headers = {'Authorization': token}
     response = requests.get(complite_url, headers=headers)
-    if response.ok:
-        is_bitlink = True
-    return is_bitlink
+    return response.ok
 
 
-if __name__ == '__main__':
+def main():
     load_dotenv()
     token = os.environ['BITLY_TOKEN']
     user_url = input('Введите ссылку: ')
-    if is_bitlink(user_url):
+    if is_bitlink(user_url, token):
         try:
-            count_clicks = count_clicks(token, user_url)
+            clicks = count_clicks(user_url, token)
         except requests.exceptions.HTTPError:
-            print(f'Введен некорректный адрес')
+            print(f'Введен некорректный адрес1')
         else:
-            print(f'По вашей ссылке прошли {count_clicks} раз(а)')
+            print(f'По вашей ссылке прошли {clicks} раз(а)')
     else:
         try:
             shorten_bitlink = shorten_link(token, user_url)
@@ -56,6 +53,10 @@ if __name__ == '__main__':
             print(f'Введен некорректный адрес')
         else:
             print(f'Битлинк: {shorten_bitlink}')
+
+
+if __name__ == '__main__':
+    main()
 
 
 
